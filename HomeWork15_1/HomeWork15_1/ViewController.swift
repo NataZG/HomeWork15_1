e569b9f8f458a76f07ca06f9e90e56a9e117b64b
NataZG <86071128+NataZG@users.noreply.github.com>
2021-07-01 20:40:04 +0300

    Project created

---
+++ HomeWork15_1/HomeWork15_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork15_1
+//
+//  Created by Nata on 01.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
